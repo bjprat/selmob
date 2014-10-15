bc2e5e036e2b60c187c456000228407a5821ac82
bjprat <bjprat@gmail.com>
2014-10-15 13:01:31 -0400

    overhaul from github

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express')
 
 
 var app = express();
-var db;
 
 var config = {
 	"HOST"		: "ec2-54-172-205-195.compute-1.amazonaws.com",
@@ -19,19 +18,77 @@ var dbPath = "mongodb://"+
 
 var standardGreeting = 'Hello Bernie!';
 
-var greetingSchema = mongoose.Schema({
+var db;
+
+var greetingSchema;
+var Greeting;
+
+greetingSchema = mongoose.Schema({
 	sentence: String
 });
+
 var Greeting = mongoose.model('Greeting', greetingSchema);
 
-db = mongoose.connect(dbPath);
+console.log('\nattempting to connect to remote MondoDB instance on another EC2 Server '+config.HOST);
+
+if ( !(db = mongoose.connect(dbPath)) )
+	console.log('unable to connect to MongoDB at '+dbPath);
+else
+	console.log('connecting to mongo at '+dbPath);
+
+mongoose.connection.on('error', function(err){
+	console.log('database connect error '+err);
+});
 
 mongoose.connection.once('open', function(){
 	var greeting;
+
+	console.log('database '+config.DATABASE+ ' is now open on '+config.HOST);
+	
 	Greeting.find( function(err, greetings){
-		if( !greetings ){
+		if( !err && greetings ){
+			console.log(greetings.length+' grettings already exist in database');
+		}
+		else{
+			console.log('no greetings in database, creating one');
 			greeting = new Greeting({ sentence: standardGreeting });
-			greeting.save();
+			greeting.save(function (err, greetingsav){
+				if(err){
+					console('couldnt save greeting to db');
+				}
+				else{
+					console.log('new greeting '+greeting.sentence+' was successfully saved to db');
+
+					Greeting.find( function(err, greetings){
+						if ( greetings )
+							console.log('checked after save: found '+greetings.length+ ' greetings in db');
+					});
+				}
+			});
+		}
+	});
+});
+
+
+app.get('/', function(req, res){
+	var responseText = '';
+
+	console.log('received client request');
+	if( !Greeting )
+		console.log('Database not ready');
+
+	Greeting.find(function (err, greetings) {
+		if(err){
+			console.log('couldnt find a greeting in the database, error: '+err);
+			next(err);
+		}
+		else{
+			if(greetings){
+				console.log('found '+greetings.length+' greetings in the db');
+				responseText = greetings [0].sentence;
+			}
+			console.log('sending greeting to client: '+responseText);
+			res.send(responseText);
 		}
 	});
 });
